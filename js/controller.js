8b82f40ec5d4a89260d3ecb76c1994d80daed614
Becii <wallnerebecca@gmail.com>
2026-02-23 11:42:19 +0100

    Clean up model code

--- js/controller.js
+++ js/controller.js
@@ -9,70 +9,70 @@ class Controller {
             .then(response => response.json())
             .then(json => model.initializeFromJson(json));
 
-        document.querySelector("event-management").addEventListener("add-event", this.handleAddEvent);
+        document.querySelector("event-management").addEventListener("add-event", this.#handleAddEvent);
 
-        document.querySelector("detailed-event-view").addEventListener("update-event", this.handleUpdateEvent);
-        document.querySelector("detailed-event-view").addEventListener("delete-event", this.handleDeleteEvent);
-        document.querySelector("detailed-event-view").addEventListener("update-event-participants", this.handleUpdateEventParticipants);
+        document.querySelector("detailed-event-view").addEventListener("update-event", this.#handleUpdateEvent);
+        document.querySelector("detailed-event-view").addEventListener("delete-event", this.#handleDeleteEvent);
+        document.querySelector("detailed-event-view").addEventListener("update-event-participants", this.#handleUpdateEventParticipants);
 
-        document.querySelector("filter-bar").addEventListener("update-status-filter", this.handleUpdateStatusFilter);
-        document.querySelector("filter-bar").addEventListener("update-tags-filter", this.handleUpdateTagsFilter);
-        document.querySelector("filter-bar").addEventListener("update-participants-filter", this.handleUpdateParticipantsFilter);
-        document.querySelector("filter-bar").addEventListener("update-search", this.handleUpdateSearch);
+        document.querySelector("filter-bar").addEventListener("update-status-filter", this.#handleUpdateStatusFilter);
+        document.querySelector("filter-bar").addEventListener("update-tags-filter", this.#handleUpdateTagsFilter);
+        document.querySelector("filter-bar").addEventListener("update-participants-filter", this.#handleUpdateParticipantsFilter);
+        document.querySelector("filter-bar").addEventListener("update-search", this.#handleUpdateSearch);
 
 
-        document.querySelector("tag-list").addEventListener("add-tag", this.handleAddTag)
-        document.querySelector("tag-list").addEventListener("edit-tag", this.handleEditTag)
-        document.querySelector("tag-list").addEventListener("delete-tag", this.handleDeleteTag)
+        document.querySelector("tag-list").addEventListener("add-tag", this.#handleAddTag)
+        document.querySelector("tag-list").addEventListener("edit-tag", this.#handleEditTag)
+        document.querySelector("tag-list").addEventListener("delete-tag", this.#handleDeleteTag)
 
-        document.querySelector("participant-list").addEventListener("add-participant", this.handleAddParticipant)
-        document.querySelector("participant-list").addEventListener("delete-participant", this.handleDeleteParticipant)
-        document.querySelector("participant-list").addEventListener("edit-participant", this.handleEditParticipant)
+        document.querySelector("participant-list").addEventListener("add-participant", this.#handleAddParticipant)
+        document.querySelector("participant-list").addEventListener("delete-participant", this.#handleDeleteParticipant)
+        document.querySelector("participant-list").addEventListener("edit-participant", this.#handleEditParticipant)
 
-        document.querySelector("user-view").addEventListener("user-login", this.handleUserLogin);
-        document.querySelector("user-view").addEventListener("user-logout", this.handleUserLogout)
+        document.querySelector("user-view").addEventListener("user-login", this.#handleUserLogin);
+        document.querySelector("user-view").addEventListener("user-logout", this.#handleUserLogout)
 
     }
 
 
 
     /* EVENT MANAGEMENT */
-    handleAddEvent = (e) => {
+    #handleAddEvent = (e) => {
         model.addEvent(e.detail.title, e.detail.datetime, e.detail.location, e.detail.description, e.detail.icon, e.detail.tagIds);
     }
 
-    handleUpdateEvent = (e) => {
+    #handleUpdateEvent = (e) => {
         console.log("Updating event ")
         model.updateEvent(e.detail.id, e.detail.title, e.detail.datetime, e.detail.location, e.detail.description, e.detail.icon, e.detail.tagIds);
     }
 
-    handleDeleteEvent = (e) => {
+    #handleDeleteEvent = (e) => {
         model.deleteEvent(e.detail.eventId)
     }
 
-    handleUpdateEventParticipants = (e) => {
+    #handleUpdateEventParticipants = (e) => {
         model.updateEventParticipants(e.detail.eventId, e.detail.participantIds);
     }
 
     /* FILTER BAR */
-    handleUpdateStatusFilter = (e) => {
+    #handleUpdateStatusFilter = (e) => {
         model.updateStatusFilters(e.detail.selectedOptions);
     }
 
-    handleUpdateTagsFilter = (e) => {
+    #handleUpdateTagsFilter = (e) => {
         model.updateTagFilters(e.detail.selectedOptions);
     }
 
-    handleUpdateParticipantsFilter = (e) => {
+    #handleUpdateParticipantsFilter = (e) => {
         model.updateParticipantFilters(e.detail.selectedOptions);
     }
 
-    handleUpdateSearch = (e) => {
+    #handleUpdateSearch = (e) => {
         model.updateSearchFilter(e.detail.searchInput.trim())
     }
 
     /* TAG MANAGEMENT */
-    handleAddTag = (e) => {
+    #handleAddTag = (e) => {
         const tagName = e.detail.tagName.trim();
         if (!model.isValidTagName(tagName)) {
             alert("Unable to add tag: tag must be between 1 and 20 characters long")
@@ -87,7 +87,7 @@ class Controller {
         model.addTag(tagName)
     }
 
-    handleEditTag = (e) => {
+    #handleEditTag = (e) => {
         const tagName = e.detail.tagName.trim();
         if (!model.isValidTagName(tagName)) {
             alert("Unable to add tag: tag must be between 1 and 20 characters long");
@@ -102,7 +102,7 @@ class Controller {
         model.updateTag(e.detail.tagId, tagName)
     }
 
-    handleDeleteTag = (e) => {
+    #handleDeleteTag = (e) => {
         if(model.canDeleteTag(e.detail.tagId)) {
             const confirmation = confirm("Are you sure you want to delete the tag?")
 
@@ -115,7 +115,7 @@ class Controller {
     }
 
     /* PARTICIPANT MANAGEMENT */
-    handleAddParticipant = (e) => {
+    #handleAddParticipant = (e) => {
         const email = e.detail.email.toLowerCase()
         if (model.participantAlreadyExists(email)) {
             alert(`Cannot add participant: participant with email ${email} already exists`);
@@ -125,7 +125,7 @@ class Controller {
         model.addParticipant(e.detail.name, email, e.detail.avatar)
     }
 
-    handleDeleteParticipant =  (e) => {
+    #handleDeleteParticipant =  (e) => {
         const confirmation = confirm("Are you sure you want to delete the participant? This will also delete them from all events.")
 
         if (confirmation) {
@@ -133,7 +133,7 @@ class Controller {
         }
     }
 
-    handleEditParticipant = (e) => {
+    #handleEditParticipant = (e) => {
         const email = e.detail.email.toLowerCase()
         const emailTaken = model.participantAlreadyExists(email, e.detail.participantId)
 
@@ -150,11 +150,11 @@ class Controller {
     }
 
     /* USER */
-    handleUserLogin = (e) => {
+    #handleUserLogin = (e) => {
         model.login(e.detail.user)
     }
 
-    handleUserLogout = (e) => {
+    #handleUserLogout = (e) => {
         model.logout()
     }
 }
